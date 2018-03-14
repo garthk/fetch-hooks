2c577e5bb1dfa593360c2db177cd442bf810be55
Garth Kidd <garth@garthk.com>
2018-03-14 17:00:45 +1100

    Fix the typings:
    * `typings` and `types` in `package.json`
    * `index.d.ts`
    * cleaner comments
    
    Also:
    
    * `changeUrlOnly` is no longer async

---
+++ index.d.ts
@@ -0,0 +1,143 @@
+import { Agent } from 'https';
+import { Readable } from 'stream';
+
+/**
+ * Return a new API-compatible WhatWG `fetch`, as interfered with by `hooks`.
+ * When called, the returned function calls each hook in order and:
+ *
+ * - Ignores it if it returns a falsey value
+ * - Returns any response it gives
+ * - If not, continues with its replacement request
+ * - Passes through to `upstream` if no hooks are left
+ * - Rejects with an error if `upstream` is `null`
+
+ * @export
+ * @param {Fetch} upstream The upstream `fetch`, or `null`
+ * @param {...FetchHook[]} hooks Zero or more hook functions
+ * @returns {Fetch} An API-compatible `fetch`
+ */
+export function hook(upstream: Fetch, ... hooks: FetchHook[]): Fetch;
+
+/**
+ * A WhatWG-compatible `fetch` from `node-fetch`.
+ */
+export const fetch: Fetch & NodeFetchBonuses;
+
+/**
+ * Predefined hooks.
+ */
+export namespace hooks {
+    /**
+     * Dump `curl` command lines to standard error.
+     */
+    export const curl: FetchHook;
+
+    /**
+     * Handle `data:` URIs.
+     */
+    export const data: FetchHook;
+
+    /**
+     * Throw an error if the protocol isn't HTTPS.
+     */
+    export const httpsOnly: FetchHook;
+
+    /**
+     * Handle `file:` URIs.
+     */
+    export function file(options: { baseURI?: string }): FetchHook;
+
+    /**
+     * Handle `s3:` URIs.
+     */
+    export function s3(options: { baseURI?: string; acl?: string }): FetchHook;
+}
+
+/**
+ * Helpers for hook writers.
+ */
+export namespace helpers {
+    /**
+     * Determine if `request.body` matters.
+     */
+    export function bodyMatters(request: Request): boolean;
+
+    /**
+     * Return a new `Request` based on `request`, but with a different `url`.
+     */
+    export function changeUrlOnly(request: Request, url: string): Request;
+
+    /**
+     * Read a string or a buffer as a readable stream..
+     */
+    export function readString(str: string | Buffer): Readable;
+}
+
+/**
+ * A fetch hook response.
+ * Contains EITHER a modified `request` OR a final `response`.
+ * To express neither, return `null`.
+ */
+interface FetchHookResponse {
+    /** A modified `Request` for subsequent hooks or the upstream `fetch` */
+    request?: Request;
+
+    /** A final `Response` */
+    response: Response;
+}
+
+/**
+ * A `fetch` hook function.
+ */
+type FetchHook = (request: Request) => Promise<FetchHookResponse>;
+
+/**
+ * A WhatWG `fetch` function.
+ */
+type Fetch = GlobalFetch['fetch'];
+
+/**
+ * Bonus members on the `fetch` from `node-fetch`.
+ */
+interface NodeFetchBonuses {
+    /**
+     * The `node-fetch` implementation of the WhatWG `Request`.
+     */
+    Request: new(input: string | Request, init?: RequestInit) => Request;
+
+    /**
+     * The `node-fetch` implementation of the WhatWG `Response`.
+     */
+    Response: new(body?: BodyInit, init?: ResponseInit) => Response;
+}
+
+/**
+ * Global interfaces.
+ */
+declare global {
+    /**
+     * `node-fetch` extensions to the global `RequestInit`
+     */
+    interface RequestInit {
+        agent?: Agent; // TODO: figure out how to more strongly type this
+        compress?: boolean;
+        follow?: number;
+        size?: number;
+        timeout?: number;
+    }
+
+    /**
+     * `node-fetch` extensions to the global `Request`
+     */
+    interface Request {
+        agent?: Agent; // TODO: figure out how to more strongly type this
+        compress: boolean;
+        counter: number;
+        follow: number;
+        hostname: string;
+        port?: number;
+        protocol: string;
+        size: number;
+        timeout: number;
+    }
+}
